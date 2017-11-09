f9618600c35b0e1d8d2f5706a455c43554c11194
Charles Shih <shichen120@gmail.com>
2017-11-09 17:45:19 +0800

    Add code to test PPS in netperf_test.sh

--- netperf_test.sh
+++ netperf_test.sh
@@ -11,24 +11,48 @@ for host in $training_host_list; do
 	ssh -i $pem root@$host "netserver -p $port"
 done
 
+msize=1400
+
+n=0
+for host in $training_host_list; do
+	let n=n+1
+	let port=10080+n
+	tmplog=netperf.tmplog.$n
+	echo "Start netperf test at port $port on host $host"
+	netperf -H $host -p $port -t UDP_STREAM -l 10 -f m -- -m $msize &> $tmplog &
+done
+
+wait
+
+cat netperf.tmplog.* > netperf.log.1 && rm -f netperf.tmplog.*
+
+BWtx=$(grep -w "$msize" netperf.log.1 | awk '{SUM += $6};END {print SUM}')
+
 
 msize=1
 
 n=0
 for host in $training_host_list; do
-	let n=n+1
-	port=$((10080+n))
-	tmplog=netperf.tmplog.$n
-	echo "Start netperf test at port $port on host $host"
-	netperf -H $host -p $port -t UDP_STREAM -l 10 -- -m $msize &> $tmplog &
+        let n=n+1
+	let port=10080+n
+        tmplog=netperf.tmplog.$n
+        echo "Start netperf test at port $port on host $host"
+        netperf -H $host -p $port -t UDP_STREAM -l 10 -f m -- -m $msize &> $tmplog &
 done
 
 wait
 
-cat netperf.tmplog.* > netperf.log && rm -f netperf.tmplog.*
+cat netperf.tmplog.* > netperf.log.2 && rm -f netperf.tmplog.*
 
-BWtx=$(grep -w "$msize" netperf.log | awk '{SUM += $6};END {print SUM}')
+PPS=$(grep -w "$msize" netperf.log.2 | awk '{SUM += $4 / $3};END {print SUM}')
 
-cat netperf.log
+cat netperf.log.1
 echo "====="
 echo $BWtx
+
+cat netperf.log.2
+echo "====="
+echo $PPS
+
+
+
