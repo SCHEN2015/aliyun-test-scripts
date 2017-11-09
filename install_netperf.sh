b1c32853f7992c664954b2625e10e69060509128
Charles Shih <shichen120@gmail.com>
2017-11-09 15:54:43 +0800

    Update install_netperf.sh

--- install_netperf.sh
+++ install_netperf.sh
@@ -2,7 +2,9 @@
 
 # This script is used in VM
 
-wget -c "https://codeload.github.com/HewlettPackard/netperf/tar.gz/netperf-2.5.0" -O netperf-2.5.0.tar.gz
+type netperf && echo "Already installed." && exit 0
+
+curl -O https://codeload.github.com/HewlettPackard/netperf/tar.gz/netperf-2.5.0
 tar -zxvf netperf-2.5.0.tar.gz
 cd netperf-netperf-2.5.0
 ./configure && make && make install && cd ..
