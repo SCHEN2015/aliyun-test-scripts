bef3429d2638f7b4eada8e7cecc9d2fc26b125a9
Charles Shih <shichen120@gmail.com>
2017-11-10 18:08:00 +0800

    bugfix in install_netperf.sh

--- install_netperf.sh
+++ install_netperf.sh
@@ -5,6 +5,7 @@
 type netperf && echo "Already installed." && exit 0
 
 curl -O https://codeload.github.com/HewlettPackard/netperf/tar.gz/netperf-2.5.0
+mv netperf-2.5.0 netperf-2.5.0.tar.gz
 tar -zxvf netperf-2.5.0.tar.gz
 cd netperf-netperf-2.5.0
 ./configure && make && make install && cd ..
