78f72ce5e0ab4de2ad41cb2799ac6f61c4dd3e35
Charles Shih <shichen120@gmail.com>
2017-11-13 11:16:47 +0800

    Add setup_environment.sh

---
+++ setup_environment.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# This script run on test host.
+# Inputs: an ip list of peer hosts
+
+
+function upload()
+{
+	host=$1
+	echo "Upload scripts to host $host"
+	scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $pem install_netperf.sh enable_rps.sh check_environment.sh root@$host:~ &
+}
+
+function setup()
+{
+	host=$1
+	echo "Setup environment on host $host"
+	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $pem root@$host "~/install_netperf.sh" &
+	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $pem root@$host "~/enable_rps.sh" &
+}
+
+function check()
+{
+	host=$1
+	echo "Check environment on host $host"
+	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $pem root@$host "~/check_environment.sh"
+}
+
+
+# main
+pem=~/cheshi_aliyun.pem
+peer_host_list=${@:-"172.20.213.194 172.20.213.192"}
+logfile=./netperf.full.log
+vmsize="Unknown"
+
+
+for host in $peer_host_list; do
+	upload $host
+	wait
+	setup $host
+	wait
+	check $host
+done
+
+exit 0
