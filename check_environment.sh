5cb11423b0ac968008e91f17511b290032ccd612
Charles Shih <shichen120@gmail.com>
2017-11-16 10:03:46 +0800

    Refactory check_environment.sh

--- check_environment.sh
+++ check_environment.sh
@@ -5,26 +5,42 @@
 # check netperf
 type netperf &>/dev/null
 if [ $? -eq 0 ]; then
-    $res1="PASS"
+    res_np="PASS"
 else
-    $res1="FAIL"
+    res_np="FAIL"
+fi
+
+# check iperf3
+type iperf3 &>/dev/null
+if [ $? -eq 0 ]; then
+    res_i3="PASS"
+else
+    res_i3="FAIL"
 fi
 
 # check multiple queue
 if [ "$(ethtool -l eth0 | grep "^Combined:" | sort -u | wc -l)" = "1" ]; then
-    $res2="PASS"
+    res_nq="PASS"
 else
-    $res2="FAIL"
+    res_nq="FAIL"
+fi
+
+# check IRQ balance
+systemctl status irqbalance | grep "active (running)" &>/dev/null
+if [ $? -eq 0 ]; then
+    res_ir="PASS"
+else
+    res_ir="FAIL"
 fi
 
 # check rps
 if [ "$(grep f /sys/class/net/eth0/queues/rx-*/rps_cpus | wc -l)" != "0" ]; then
-    $res3="PASS"
+    res_rp="PASS"
 else
-    $res3="FAIL"
+    res_rp="FAIL"
 fi
 
 # Show summary
-echo "Netperf:$res1 NIC_Queue:$res2 RPS:$res3"
+echo "Netperf:$res_np Iperf3:$res_i3 NIC_Queue:$res_nq IRQ_Bal:$res_ir RPS:$res_rp"
 
 exit 0
