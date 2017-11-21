1c8822807d3896a7ba35e845892bd74721df4837
Charles Shih <shichen120@gmail.com>
2017-11-21 17:11:34 +0800

    Enhance install_netperf.sh

--- network_pt/install_netperf.sh
+++ network_pt/install_netperf.sh
@@ -4,14 +4,17 @@
 
 type netperf && echo "Already installed." && exit 0
 
-curl -O https://codeload.github.com/HewlettPackard/netperf/tar.gz/netperf-2.5.0
-mv netperf-2.5.0 netperf-2.5.0.tar.gz
-tar -zxvf netperf-2.5.0.tar.gz
-cd netperf-netperf-2.5.0
-./configure && make && make install && cd ..
+#curl -O https://codeload.github.com/HewlettPackard/netperf/tar.gz/netperf-2.5.0
+#mv netperf-2.5.0 netperf-2.5.0.tar.gz
+#tar -zxvf netperf-2.5.0.tar.gz
+#cd netperf-netperf-2.5.0
+#./configure && make && make install && cd ..
+
+
+curl -O ftp://rpmfind.net/linux/mageia/distrib/3/x86_64/media/core/release/netperf-2.5.0-2.mga3.x86_64.rpm
+yum localinstall netperf-2.5.0-2.mga3.x86_64.rpm -y
 
 netperf -h
 netserver -h 
 
 exit 0
-
